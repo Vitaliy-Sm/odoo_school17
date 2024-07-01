3b0e0ed639b6cf5a4c8ad3c98d85acc5350566a0
VitaliySm <vsmokvin@gmail.com>
2024-07-01 11:20:48 +0300

    [IMP] TASK 4_2

--- hr_hospital/models/hospital_patient.py
+++ hr_hospital/models/hospital_patient.py
@@ -1,7 +1,7 @@
 from datetime import date
 from dateutil.relativedelta import relativedelta
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
 class HospitalPatient(models.Model):
@@ -13,10 +13,38 @@ class HospitalPatient(models.Model):
         comodel_name='hospital.doctor',
         index=True,
     )
+    visit_history_ids = fields.One2many(
+        comodel_name='hospital.patient.visits',
+        inverse_name='patient_id',
+    )
+
     birthday_date = fields.Date(string='Date of Birth')
     age = fields.Integer(compute="_compute_age")
     passport = fields.Char()
     contact = fields.Char()
+
+    def action_open_visit_history(self):
+        return {
+            'name': _('Visit History'),
+            'view_mode': 'tree',
+            'view_id': False,
+            'view_type': 'form',
+            'res_model': 'hospital.patient.visits',
+            'type': 'ir.actions.act_window',
+            'domain': [('patient_id', '=', self.id)],
+        }
+
+    def action_create_visit(self):
+        return {
+            'name': _('Create Visit'),
+            'view_mode': 'form',
+            'view_id': False,
+            'view_type': 'form',
+            'res_model': 'hospital.patient.visits',
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+            'context': {'default_patient_id': self.id},
+        }
 
     @api.depends('birthday_date')
     def _compute_age(self):
